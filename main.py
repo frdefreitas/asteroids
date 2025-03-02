b0fae4e6eff4010b38eae127de4fb2d9a8d48d81
frdefreitas <77462253+frdefreitas@users.noreply.github.com>
2025-03-02 11:58:10 -0300

    initial status asteroids pygame

---
+++ main.py
@@ -0,0 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+# import the connect_database function
+# and the database_version variable
+# from database.py into the current file
+#from database import connect_database, database_version
+
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
