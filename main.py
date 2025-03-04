87e114e49a957a388ac6b1af8c7482677e9d6ef0
frdefreitas <77462253+frdefreitas@users.noreply.github.com>
2025-03-04 12:01:21 -0300

    add player cursor on screen, rotate and move methods

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ import pygame
 #from database import connect_database, database_version
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+            clock.tick(60)
+            dt = clock.tick(60) / 1000
+            screen.fill((0, 0, 0))
+            player.update(dt)
+            player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
